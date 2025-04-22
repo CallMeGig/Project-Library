19e0a0550f44f9498f9468802f77aade81a1bf6e
git <73262104+CallMeGig@users.noreply.github.com>
2025-04-23 00:07:27 +0200

    Add functionality to remove book entries

--- exercise.js
+++ exercise.js
@@ -3,6 +3,10 @@ const showBtn = document.querySelector("#showDialog");
 const dialog = document.querySelector(".dialog");
 const closeDialog = document.querySelector("#closeDialog");
 
+addBookToLibrary("Harry potter", "J.K. Rowling", 254, true);
+addBookToLibrary("Atomic Habits", "James Clear", 256, false);
+
+displayLibraryBookCards(myLibrary);
 
 function Book(id, title, author, pages, read) {
     if (!new.target) {
@@ -40,61 +44,16 @@ function displayLibraryBookCards(library) {
         // DOM
         container.appendChild(bookCard)
     }
-
-        // console
-        // console.log(`
-        //     -----------------------------
-        //     book ${parseInt(index) + 1}
-        //     -----------------------------
-        //     Title: ${book.title}
-        //     Author: ${book.author}
-        //     Pages: ${book.pages}
-        //     Status: ${book.read}
-            
-        //     ID: ${book.id}
-        //     ------------------------------`);
+    console.log(myLibrary);
 }
 
-function createLibraryBookCard(book) {
-    // creates a card with all the books
-    const bookCard = document.createElement("div");
-        
-    const title = document.createElement("p");
-    const author = document.createElement("p");
-    const pages = document.createElement("p");
-    const status = document.createElement("p");
-    const idNum = document.createElement("p");
-    const cardItems = [title, author, pages, status, idNum];
 
-    bookCard.classList.add("bookCard");
-    title.classList.add("title");
-    author.classList.add("author");
-    pages.classList.add("pages");
-    status.classList.add("status");
-    idNum.classList.add("idNum");
-    
-    title.textContent = `Title: ${book.title}`;
-    author.textContent = `Author: ${book.author}`;
-    pages.textContent = `Pages: ${book.pages}`;
-    status.textContent = `Status: ${book.read}`;
-    idNum.textContent = `ID: ${book.id}`;
-
-
-    for (let item in cardItems) {
-        bookCard.appendChild(cardItems[item]);
-    }
-
-    return bookCard;
-}
 
 function getBookDetails(form) {
-    const title = form.title.value;
-    const author = form.author.value;
-    const pages = form.pages.value;
-    const read = form.read.checked;
-
-    return [title, author, pages, read];
+    return [form.title.value, form.author.value, form.pages.value, form.read.checked];
 }
+
+
 
 function clearDialog(form) {
     form.title.value = '';
@@ -110,10 +69,9 @@ showBtn.addEventListener("click", () => {
 
 closeDialog.addEventListener("click", (e) => {
     const form = document.querySelector("#newBookForm");
-    const newBookArray = getBookDetails(form);
+    const [title, author, pages, read] = getBookDetails(form);
 
-
-    addBookToLibrary(newBookArray[0], newBookArray[1], newBookArray[2], newBookArray[3]);
+    addBookToLibrary(title, author, pages, read);
     displayLibraryBookCards(myLibrary);
 
     clearDialog(form);
@@ -121,7 +79,70 @@ closeDialog.addEventListener("click", (e) => {
     dialog.close();
 });
 
-addBookToLibrary("Harry potter", "J.K. Rowling", 254, true);
-addBookToLibrary("Atomic Habits", "James Clear", 256, false);
+function deleteBook(index, domBookCard) {
+    myLibrary.splice(index, 1);
+    domBookCard.remove();
+    displayLibraryBookCards(myLibrary);
 
-displayLibraryBookCards(myLibrary);
+}
+
+
+
+
+function createLibraryBookCard(book) {
+    // creates a card with all the books
+    const bookCard = document.createElement("div");
+        
+    const deleteCard = document.createElement("button");
+    const deleteIcon = document.createElement("img");
+    const title = document.createElement("p");
+    const author = document.createElement("p");
+    const pages = document.createElement("p");
+    const status = document.createElement("p");
+    const idNum = document.createElement("p");
+    const cardItems = [deleteCard, title, author, pages, status, idNum];
+
+    bookCard.classList.add("bookCard");
+    deleteCard.classList.add("deleteCard");
+    deleteIcon.classList.add("deleteIcon");
+    title.classList.add("title");
+    author.classList.add("author");
+    pages.classList.add("pages");
+    status.classList.add("status");
+    idNum.classList.add("idNum");
+    
+    bookCard.setAttribute("id", book.id);
+    deleteIcon.setAttribute("src", "assets/trash-can-outline.png");
+    
+    deleteCard.appendChild(deleteIcon);
+
+    title.textContent = `${book.title}`;
+    author.textContent = `by ${book.author}`;
+    pages.textContent = `Pages: ${book.pages}`;
+    status.textContent = `Status: ${book.read}`;
+    // idNum.textContent = `ID: ${book.id}`;
+
+
+    for (let item in cardItems) {
+        bookCard.appendChild(cardItems[item]);
+    }
+
+    deleteCard.addEventListener("click", () => removeBookEntrys(deleteCard))
+    
+    return bookCard;
+}
+
+function removeBookEntrys(cardElement) {
+    // 
+    // function gets the id property from the given book card element,
+    // finds the corresponding book object in the library array and,
+    // calls deletebook() to delete both entries.
+    //  
+
+    const idToDel = cardElement.parentElement.id; 
+    const bookInArr = myLibrary.find(book => book.id === idToDel); 
+    const targetIndex = myLibrary.indexOf(bookInArr);
+
+    deleteBook(targetIndex, cardElement);
+    
+}
